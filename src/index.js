e0e40951157f3d399bb607af694ead15cdb93614
tgrud <tim@timgrudowski.com>
2022-09-24 00:01:12 -0500

    Adjust render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// import ReactDOM from 'react-dom' // <-- tutorial
-import ReactDOM from 'react-dom/client'; // <-- current npm
+import ReactDOM from 'react-dom' // <-- tutorial
+// import ReactDOM from 'react-dom/client'; // <-- current npm
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
